a7889520b925153a0ba324c6f0358efe3463c757
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-05-12 11:46:54 -0300

    Agregar archivos de proyecto.

---
+++ ApiFotos/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiFotos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFotos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<MarvelFotos>Fotos{get; set;}
+    }
+}
---
+++ ApiFotos/Models/MarvelFotos.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiFotos.Models
+{
+    public class MarvelFotos
+    {
+        public  int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set;}
+    }
+}
