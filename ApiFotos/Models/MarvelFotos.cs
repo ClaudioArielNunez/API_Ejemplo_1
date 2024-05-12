d899560c45213dacf5d24da1de8cad38357db585
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-05-12 11:49:15 -0300

    DataAnnotations en Model

--- ApiFotos/Models/MarvelFotos.cs
+++ ApiFotos/Models/MarvelFotos.cs
@@ -1,10 +1,15 @@
-﻿namespace ApiFotos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFotos.Models
 {
     public class MarvelFotos
     {
+        [Key]
         public  int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
+        [Required]
         public string ImagenUrl { get; set;}
     }
 }
